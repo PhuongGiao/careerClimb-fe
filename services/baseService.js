eda1880a7f5b87ba787d94ec4fe0ef5551dd6e85
Giao <ppgiao2001@gmail.com>
2023-06-29 19:59:41 +0700

    a

--- services/baseService.js
+++ services/baseService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3003/api";
+const baseUrl = "https://careerclimb-be.onrender.com/api";
 
 export class BaseService {
   put = (url, model) => {
